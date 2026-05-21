43c899abcb51423dfbeff4ef19fe0b7e9ef27bea
Sanket <sanketpandia@gmail.com>
2026-05-21 18:46:03 +0530

    Update registration API path

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -64,7 +64,7 @@ export class ApiService {
                 last_flight: lastFlight
             };
 
-            const res = await fetch(`${API_URL}/api/v1/pilots/register`, {
+            const res = await fetch(`${API_URL}/api/v1/user/register`, {
                 method: "POST",
                 headers: {
                     ...generateRegistrationMetaHeaders(meta),
